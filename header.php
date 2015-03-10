c9f501590361deb20252995be9d916b28cf526f9
milanju <Milan.Jurhan@live.de>
2015-03-10 13:41:20 +0100

    update button styling

--- header.php
+++ header.php
@@ -37,16 +37,16 @@ $(document).ready(function() {
 	<!--Form for Login-->
 	<form action="" method="POST">
 	<?php if ((empty($_COOKIE["rememberUsername"]))) { ?>
-	<input type="text" name="login_name" style="width: 80px;color:#787878 ;border:solid 1px #787878;" value="User Name" onFocus="value=''; this.style.color = 'black';">
+	<input type="text" name="login_name" class="login-input" style="width: 80px;color:#787878 ;border:solid 1px #787878;" value="User Name" onFocus="value=''; this.style.color = 'black';">
 	<input type="password" name="login_pw" style="width: 80px;color:#787878 ;border:solid 1px #787878 ;" value="Password" onFocus="value=''; this.style.color = 'black';">
-	<input type="submit" name="login_user" value="Login" style="color:black;">
-	<input type="submit" name="register"value="Register" style="color:black;">
+	<input type="submit" name="login_user" class="login-button" value="Login">
+	<input type="submit" name="register" class="register-button" value="Register">
 		<?php if (!(empty($_COOKIE["invalidLogin"]))) { ?>
 			<div id="invalid_login_message">Invalid username or password.</div>
-		<?php 
+		<?php
 		} ?>
 	<?php } else { ?>
-	<input type="submit" name="logout" value="Logout" style="color:black;">
+	<input type="submit" name="logout"  class="logout-button" value="Logout">
 	<div id="welcome_message">Hello <?php echo $_COOKIE["rememberUsername"]; ?> ! </div><?php } ?>
 	</form>
 </div>
@@ -69,9 +69,9 @@ nsec   = d.getSeconds();
 
 if(nyear<1000) nyear=nyear+1900;
 
-     if(nhour ==  0) {ap = " AM";nhour = 12;} 
-else if(nhour <= 11) {ap = " AM";} 
-else if(nhour == 12) {ap = " PM";} 
+     if(nhour ==  0) {ap = " AM";nhour = 12;}
+else if(nhour <= 11) {ap = " AM";}
+else if(nhour == 12) {ap = " PM";}
 else if(nhour >= 13) {ap = " PM";nhour -= 12;}
 
 if(nmin <= 9) {nmin = "0" +nmin;}
@@ -86,12 +86,12 @@ window.onload=GetClock;
 <div id="clockbox"></div>
 
 	</div>
-	
+
 </div>
 
 <div id="header">
 
-<?php 
+<?php
 $rnd_var=rand(1, 30);
 if($rnd_var==1){ ?>
 <a href="index.php"><img src="images/banner_k.png" width="1100" height="140"  alt="Home"></a>
