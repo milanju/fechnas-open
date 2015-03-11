11eb7bb9410da1f77ad972481c8107a6808868ed
milanju <Milan.Jurhan@live.de>
2015-03-11 07:36:07 +0100

    refactor code

--- modules/bracket.php
+++ modules/bracket.php
@@ -1,8 +1,8 @@
 <div id="bracket_div" class="content-module">
-<div class="title">
-	<div class="title-text">Bracket</div>
-</div>
-<?php
+	<div class="title">
+		<div class="title-text">Bracket</div>
+	</div>
+	<?php
 	$bracket=getSpecificValue($con, 'data', 'data', 'value', 'bracket');
 	$variable=1600;
 	//get bracket_size
@@ -34,14 +34,11 @@
 	echo "<table class='bracket_table'>";
 	echo "<tr>";
 
-
 	$td_height=$bracket_size*25+10;
 	for($i=$bracket_size;$i>=1;$i=$i/2){
-
 		echo "<th class='bracket_th'>";
 		echo "ro".$i ;
 		echo "</th>";
-
 	}
 	echo "</tr>";
 	echo "<tr>";
@@ -62,114 +59,105 @@
 
 
 		for($j=1;$j<=$i;$j=$j+2){
-		if(($i != $bracket_size || $j != 1)){
+			if(($i != $bracket_size || $j != 1)){
 
-		}
-		if($j != 1) {
-			$top_position+=$li_margin2;
-			echo "<li class='bracket_match' style='top:";
-			echo $top_position;
-			echo "px'>";
-			?>
-			<?php
-		} else{
-			if($i!=1){
-			$top_position+=($li_margin2/2)-15;
-			echo "<li class='bracket_match' style='top:";
-			echo $top_position;
-			echo "px'>";
-			}else{
-				$top_position+=(($li_margin2/2)-15)/2;
+			}
+
+			if($j != 1) {
+				$top_position+=$li_margin2;
 				echo "<li class='bracket_match' style='top:";
 				echo $top_position;
 				echo "px'>";
+			} else {
+				if($i!=1) {
+				$top_position+=($li_margin2/2)-15;
+				echo "<li class='bracket_match' style='top:";
+				echo $top_position;
+				echo "px'>";
+				} else {
+					$top_position+=(($li_margin2/2)-15)/2;
+					echo "<li class='bracket_match' style='top:";
+					echo $top_position;
+					echo "px'>";
+				}
 			}
-		}
 
 			echo "<form action='' method='POST'>";
 			echo "<input type='text' name='ro16_1' value=" .  getValue($con, $bracket, $j)  . " class='bracketfield' readonly>";
-
 			if($bracket != bracket_ro1){
 				if (getSpecificValue($con, 'users', 'name', 'rights', $_COOKIE["rememberUsername"])=='superadmin') {
-				echo "<input class = 'submit_win_button' type='submit' name='$i*$j' value='W'>";
+					echo "<input class = 'submit_win_button' type='submit' name='$i*$j' value='W'>";
 				} else{
-				//Score
-				echo "<input type='text' name='$i*$j*n' value='' class='score_field'>";
+					//Score
+					echo "<input type='text' name='$i*$j*n' value='' class='score_field'>";
 				}
 			}
-			echo "</form>";
+				echo "</form>";
 
 			if($bracket != bracket_ro1){
-
 				echo "<form action='' method='POST'>";
 				echo "<input type='text' name='ro16_1' value=" .  getValue($con, $bracket, $j+1)  . " class='bracketfield' readonly>";
 				$k=$j+1;
 				if (getSpecificValue($con, 'users', 'name', 'rights', $_COOKIE["rememberUsername"])=='superadmin') {
-				echo "<input class = 'submit_win_button' type='submit' name='$i*$k' value='W'>";
+					echo "<input class = 'submit_win_button' type='submit' name='$i*$k' value='W'>";
 				} else{
-				//Score
-
-				echo "<input type='text' name='$i*$k*n' value='' class='score_field'>";
-
+					//Score
+					echo "<input type='text' name='$i*$k*n' value='' class='score_field'>";
 				}
 			}
 			echo "</form>";
 			echo "</li>";
-
-
-
 		}
+
 		echo "</ul>";
-
-		?>
-		<?php
-
-		$margin_top_bottom=10;
-		$blc_height=($td_height-50*$gangsta_var*2)+30;
-		$blc_margin=($td_height-$blc_height)/2;
-		$blc_margin_top=$blc_margin-2;
-		$blc_margin_bottom=$blc_margin+2;
-		if($i==$bracket_size){
-			$blc_height=$td_height-20;
-			$blc_margin_top=8;
-			$blc_margin_bottom=12;
+		$margin_top_bottom = 10;
+		$blc_height = ($td_height - 50 * $gangsta_var * 2) + 30;
+		$blc_margin = ($td_height - $blc_height) / 2;
+		$blc_margin_top = $blc_margin - 2;
+		$blc_margin_bottom = $blc_margin + 2;
+		if($i == $bracket_size){
+			$blc_height = $td_height - 20;
+			$blc_margin_top = 8;
+			$blc_margin_bottom = 12;
 		}
 
-		if($i!=1){
+		if($i != 1){
 		?>
 		<div id="randomrectangle" style="height:<?php echo "$td_height"; ?>px;">
-		<div id="bracket_line_container" style="height:<?php echo "$blc_height"; ?>px;
-			margin-top:<?php echo "$blc_margin_top"; ?>px;
-			margin-bottom:<?php echo "$blc_margin_bottom"; ?>px;">
+			<div id="bracket_line_container" style="height:<?php echo "$blc_height"; ?>px;
+				margin-top:<?php echo "$blc_margin_top"; ?>px;
+				margin-bottom:<?php echo "$blc_margin_bottom"; ?>px;">
 
-			<?php for($k=0;$k<$i;$k+=4){
-			echo "<div id='bracket_border'>";
-			echo "<div id='bracket_line'>";
-			echo "<div id='top_line'>";
-			echo "</div>";
-			echo "<div id='bottom_line'>";
-			echo "</div>";
+				<?php
+				for($k=0;$k<$i;$k+=4){
+					echo "<div id='bracket_border'>";
+					echo "<div id='bracket_line'>";
+					echo "<div id='top_line'>";
+					echo "</div>";
+					echo "<div id='bottom_line'>";
+					echo "</div>";
 
-			echo "</div>";
-			echo "<div id='first_border'>";
-			echo "<div id='top_border'></div>";
-			echo "<div id='right_border'></div>";
-			echo "</div>";
-			echo "</div>";
+					echo "</div>";
+					echo "<div id='first_border'>";
+					echo "<div id='top_border'></div>";
+					echo "<div id='right_border'></div>";
+					echo "</div>";
+					echo "</div>";
 
-			 } ?>
+				}
+				?>
 
 
 
 
-		</div>
+			</div>
 		</div>
 
 		<?php
 		}
 
 		echo "</td>";
-			//mod bracket ~~~
+		//mod bracket ~~~
 		switch($bracket){
 			case 'bracket_ro64':
 				$bracket='bracket_ro32';
@@ -195,7 +183,7 @@
 		$li_margin=$li_margin*2+42;
 		$margin_counter++;
 		if($i != $bracket_size){
-		$gangsta_var*=2;
+			$gangsta_var*=2;
 		}
 
 	}
