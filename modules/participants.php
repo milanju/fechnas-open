6b91671ab6b027fb53bf0698c304ffdcdc89cabe
milanju <Milan.Jurhan@live.de>
2015-03-11 07:43:13 +0100

    refactor code

--- modules/participants.php
+++ modules/participants.php
@@ -1,36 +1,20 @@
 <div id="participants" class="content-module">
-<div class=title>
-  <div class="title-text">Participants</div>
-</div>
+  <div class=title>
+    <div class="title-text">Participants</div>
+  </div>
+  <br>
+  <?php
+  /*
+   *Create list of participants, extracted from 'participants' SQL table
+   *and sorted by alphabet, participants seperated by comma.
+   */
+  $participants_array=getValueArray($con, 'participants');
+  sort($participants_array);
+  for($i = 1; $i <= count(getValueArray($con, 'participants')); $i++){
 
-
-
-<br>
-
-<?php
-
-
-
-/*
- *Create list of participants, extracted from 'participants' SQL table
- *and sorted by alphabet, participants seperated by comma.
- */
-$participants_array=getValueArray($con, 'participants');
-sort($participants_array);
-for($i=1;$i<=count(getValueArray($con, 'participants'));$i++){
-
-	if($i!=1)echo ", ".$participants_array[$i-1];
-	else echo $participants_array[$i-1];
-}
-
-
-
-?>
-
-
-<br>
-
-
-
-
+  	if($i != 1) echo ", ". $participants_array[$i - 1];
+  	else echo $participants_array[$i-1];
+  }
+  ?>
+  <br>
 </div>
