bcff691f37f1f8fb6c9d5ded9d9c008dff6684f1
milanju <Milan.Jurhan@live.de>
2015-03-14 17:41:18 +0100

    refactor code

--- functions.php
+++ functions.php
@@ -13,23 +13,23 @@ function generateBracket($con, $shuffledParticipantsArray)
     $bracket_size=getBracketSize($bracket);
     $byeCount = $bracket_size-count($shuffledParticipantsArray);
     $byeCount2 = $byeCount;
-    $slot=1;
-    $array_pos=0;
+    $slot = 1;
+    $array_pos = 0;
     for ($i = 0; $i < $bracket_size; $i++) {
         if($byeCount > 0){
             insertInTable($con, $bracket, 'spot', 'name', $slot, '*BYE*');
             $slot+=2;
             $byeCount--;
         } else {
-            if ($array_pos==0){
+            if ($array_pos == 0){
                 $slot=2;
             }
             insertInTable($con, $bracket, 'spot', 'name',
                 $slot, $shuffledParticipantsArray[$array_pos]);
             $array_pos++;
-            $slot+=2;
-            if($slot==$bracket_size+2){
-                $slot=$byeCount2*2+1;
+            $slot += 2;
+            if($slot == $bracket_size + 2){
+                $slot=$byeCount2 * 2 + 1;
             }
         }
     }
