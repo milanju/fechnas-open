1fef46574f7a27105956a0e83dfc8c509cff8625
milanju <Milan.Jurhan@live.de>
2015-03-15 12:01:13 +0100

    refactor html

--- register.php
+++ register.php
@@ -1,9 +1,18 @@
 <?php include ("header.php"); ?>
-
 <div id="content">
-<form action="milantowns.php" method="POST">
-<input type="text" name="register_name" style="width: 80px;color:#787878 ;border:solid 1px #787878;" value="User Name" onFocus="value=''; this.style.color = 'black';"> (Max. 20 Letters) <br>
-<input type="password" name="register_pw" style="width: 80px;color:#787878 ;border:solid 1px #787878 ;" value="Password" onFocus="value=''; this.style.color = 'black';"> (Max. 20 Letters) <br>
-<input type="submit" name="register_user" value="Register" style="color:black;">
+  <form action="milantowns.php" method="POST">
+    <input type="text" name="register_name"
+        style="width: 80px; color:#787878; border:solid 1px #787878"
+        value="User Name" onFocus="value=''; this.style.color = 'black';">
+    (Max. 20 Letters)
+    <br>
+    <input type="password" name="register_pw"
+        style="width: 80px; color:#787878; border:solid 1px #787878;"
+        value="Password" onFocus="value=''; this.style.color = 'black';">
+    (Max. 20 Letters)
+    <br>
+    <input type="submit" name="register_user"
+        value="Register" style="color:black;">
+  </form>
 </div>
 <?php include ("footer.php"); ?>
